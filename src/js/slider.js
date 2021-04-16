0eeeb3cf5aa329d02b5224844d71a198a9069213
nurlasova <nurlasova.kg@gmail.com>
2021-04-16 09:35:47 +0600

    I added new sections

---
+++ src/js/slider.js
@@ -0,0 +1,10 @@
+const swiper1 = new Swiper('.swiper-container', {
+    // Optional parameters
+      loop: true,
+      slidesPerView: 5,
+      slidesPerGroup:1,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+  
+  });
